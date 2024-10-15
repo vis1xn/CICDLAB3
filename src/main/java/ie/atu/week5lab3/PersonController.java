7995c09af5f813c42e36f24a173f19cbb47dea40
vis1xn <g00421432@atu.ie>
2024-10-15 11:06:48 +0100

    fixed code

--- src/main/java/ie/atu/week5lab3/PersonController.java
+++ src/main/java/ie/atu/week5lab3/PersonController.java
@@ -1,14 +1,23 @@
 package ie.atu.week5lab3;
 
 import jakarta.validation.Valid;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
+@RequestMapping("/person")
 public class PersonController {
-    @PostMapping("/person/createPerson")
-    public String createPerson(@RequestBody @Valid Person userRequest){
-        return "Details added";
+    List<Person>personList = new ArrayList<>();
+    @GetMapping("/person")
+    private List<Person> getPerson(){
+        return personList;
+    }
+
+    @PostMapping("/createPerson")
+    public List<Person>createPerson(@RequestBody @Valid Person person){
+        personList.add(person);
+        return personList;
     }
 }
