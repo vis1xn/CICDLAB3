21ee948b884fdccf88893a17dd4978d2a421fa46
vis1xn <g00421432@atu.ie>
2024-10-15 09:48:10 +0100

    Started new lab

---
+++ src/main/java/ie/atu/week5lab3/Week5Lab3Application.java
@@ -0,0 +1,13 @@
+package ie.atu.week5lab3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week5Lab3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Week5Lab3Application.class, args);
+	}
+
+}
---
+++ src/test/java/ie/atu/week5lab3/Week5Lab3ApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.week5lab3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week5Lab3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
